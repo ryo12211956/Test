62cef46ef721a39dfdfa602a0c062b69873a5cf8
ryo12211956 <96876520+ryo12211956@users.noreply.github.com>
2022-02-15 21:07:43 +0900

    Add files via upload

---
+++ make_array_1.php
@@ -0,0 +1,8 @@
+<pre>
+<?php
+ //array関数で配列を作成する
+ $friends = array("はるき","かおる","ひでと");
+ var_dump($friends); //配列の中身を確認
+ ?>
+ </pre>
+ 
---
+++ switch2.php
@@ -0,0 +1,12 @@
+<?php
+switch ($i = 15) {
+    case 10: 
+    echo "10";
+    break;
+    case 20:
+    echo "20";
+    break;
+    default:
+    echo "その他";
+}
+?>
